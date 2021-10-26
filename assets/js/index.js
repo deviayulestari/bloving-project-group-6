218ac8fe8478ed923d011bc98cfdbcfa3824a9f1
deviayulestari <devialestari04@gmail.com>
2021-10-26 15:30:40 +0700

    updating detail-blog

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 import { isLogin, logout, getUser} from "./helpers.js";
+import dataBlog from "./data-blogs.js";
 
 const loginBtnEl = document.getElementById("loginBtn");
 const logoutBtnEl = document.getElementById("confirmLogout");
@@ -21,3 +22,31 @@ loginBtnEl.addEventListener('click', () => {window.location.href = "../pages/log
 logoutBtnEl.addEventListener('click', () => {
     logout();
 });
+
+
+//logic latest blog card
+const elLatestBlog = document.querySelector(".latest-blog")
+
+dataBlog.forEach((latest, id)=>{
+    if(id < 3){
+    const elCol = `
+        <div class="col-lg-4 col-sm-12">
+            <div class="box p-3 m-2">
+                <div class="image">
+                    <img src="${latest.img_url}" class="w-100 h-100"  alt="Blog 1 Image">
+                    </div>
+                <div class="content p-2">
+                    <h3>${latest.title}</h3>
+                    <a href="${'../pages/detail-blog.html?id=' + latest.id}" class="link-btn">read more</a>
+                    <div class="icons">
+                        <a href="#"><i class="fas fa-user"></i>by ${latest.author}</a>
+                        <a href="#"><i class="fas fa-calendar"></i>${latest.date}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        elLatestBlog.insertAdjacentHTML('beforeend', elCol);
+    }
+    console.log(elLatestBlog);
+})
